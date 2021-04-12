196c07fb227d072c56d046975b769e00518caa93
Dev <devenlukeday@gmail.com>
2021-04-12 09:37:41 -0600

    adds crud functions for plist

--- Todoey/Controllers/TodoListTableViewController.swift
+++ Todoey/Controllers/TodoListTableViewController.swift
@@ -12,14 +12,13 @@ class TodoListTableViewController: UITableViewController {
     
     //MARK: - Properties
     var itemArray = [Item]()
-    
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     //MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupItemArray()
+        loadItems()
     }
     
     //MARK: - Actions
@@ -28,17 +27,6 @@ class TodoListTableViewController: UITableViewController {
     }
     
     //MARK: - Helper Functions
-    func setupItemArray() {
-        
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
-        
-        if let  items = defaults.array(forKey: "ToDoListArray") as? [Item] {
-            itemArray = items
-        }
-    }
-    
     func addItem() {
         
         var textField = UITextField()
@@ -49,14 +37,14 @@ class TodoListTableViewController: UITableViewController {
             
             let newItem = Item()
             newItem.title = textField.text ?? ""
-        
+            
             self.itemArray.append(newItem)
             
-            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            //            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             
-            self.tableView.reloadData()
+            self.saveItems()
         }
-  
+        
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
             textField = alertTextField
@@ -65,6 +53,32 @@ class TodoListTableViewController: UITableViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK: - CRUD
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do{
+            let encoder = PropertyListEncoder()
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+            
+        } catch {
+            print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+            }
+        }
     }
     
     // MARK: - Table view data source
@@ -88,8 +102,8 @@ class TodoListTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-    
-        tableView.reloadData()
+        
+        saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
