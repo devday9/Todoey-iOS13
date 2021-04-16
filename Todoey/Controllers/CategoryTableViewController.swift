c2cac912924e8583a0bed8c777055437e963d83f
Dev <devenlukeday@gmail.com>
2021-04-16 11:11:52 -0600

    adds category code

--- Todoey/Controllers/CategoryTableViewController.swift
+++ Todoey/Controllers/CategoryTableViewController.swift
@@ -2,7 +2,7 @@
 //  CategoryTableViewController.swift
 //  Todoey
 //
-//  Created by Deven Day on 4/14/21.
+//  Created by Deven Day on 4/15/21.
 //  Copyright © 2021 App Brewery. All rights reserved.
 //
 
@@ -10,23 +10,87 @@ import UIKit
 import CoreData
 
 class CategoryTableViewController: UITableViewController {
-
+    
     //MARK: - Properties
+    var categories = [Category]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     //MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    }
-
-    //MARK: - Actions
-    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
+        loadCategories()
     }
     
-    //MARK: - TableView Datasource Methods
+    //MARK: - Actions
+    @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+        addCategory()
+    }
     
-    //MARK: - TableView Delegate Methods
+    //MARK: - Helper Functions
+    func addCategory() {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text!
+            
+            self.categories.append(newCategory)
+            
+            self.saveCategories()
+        }
+        
+        alert.addAction(action)
+        
+        alert.addTextField { (field) in
+            textField = field
+            textField.placeholder = "Add a new category"
+        }
+        
+        present(alert, animated: true, completion: nil)
+    }
     
-    //MARK: - TableView Manipulation Methods
+    //MARK: - Save
+    func saveCategories() {
+        
+        do {
+            try context.save()
+        } catch {
+            print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    //MARK: - Load
+    func loadCategories() {
+        
+        let request: NSFetchRequest<Category> = Category.fetchRequest()
+        
+        do {
+            categories = try context.fetch(request)
+        } catch {
+            print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
+        
+        cell.textLabel?.text = categories[indexPath.row].name
+        
+        return cell
+    }
 }//END OF CLASS
